47f9890245dd043a205559171bff1a0cd1973a39
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2020-02-06 15:39:53 +0400

    Added money substruction and it's tests

--- SnackMachine.Logic/Money.cs
+++ SnackMachine.Logic/Money.cs
@@ -38,13 +38,19 @@ namespace SnackMachine.Logic
         #endregion
 
         #region Properties
-        public int OneCentCount { get; private set; }
-        public int TenCentCount { get; private set; }
-        public int QuarterCount { get; private set; }
-        public int OneDollarCount { get; private set; }
-        public int FiveDollarCount { get; private set; }
-        public int TwentyDollarCount { get; private set; }
+        public int OneCentCount { get; }
+        public int TenCentCount { get; }
+        public int QuarterCount { get; }
+        public int OneDollarCount { get; }
+        public int FiveDollarCount { get; }
+        public int TwentyDollarCount { get; }
 
+        public decimal Amount => OneCentCount * 0.01m +
+                                TenCentCount * 0.1m +
+                                QuarterCount * 0.25m +
+                                OneDollarCount +
+                                FiveDollarCount * 5 +
+                                TwentyDollarCount * 20;
         #endregion
 
         #region Methods
@@ -52,12 +58,23 @@ namespace SnackMachine.Logic
         public static Money operator +(Money money1, Money money2)
         {
             return new Money(
-                money1.OneCentCount += money2.OneCentCount,
-                money1.TenCentCount += money2.TenCentCount,
-                money1.QuarterCount += money2.QuarterCount,
-                money1.OneDollarCount += money2.OneDollarCount,
-                money1.FiveDollarCount += money2.FiveDollarCount,
-                money1.TwentyDollarCount += money2.TwentyDollarCount
+                money1.OneCentCount + money2.OneCentCount,
+                money1.TenCentCount + money2.TenCentCount,
+                money1.QuarterCount + money2.QuarterCount,
+                money1.OneDollarCount + money2.OneDollarCount,
+                money1.FiveDollarCount + money2.FiveDollarCount,
+                money1.TwentyDollarCount + money2.TwentyDollarCount
+            );
+        }
+        public static Money operator -(Money money1, Money money2)
+        {
+            return new Money(
+                money1.OneCentCount - money2.OneCentCount,
+                money1.TenCentCount - money2.TenCentCount,
+                money1.QuarterCount - money2.QuarterCount,
+                money1.OneDollarCount - money2.OneDollarCount,
+                money1.FiveDollarCount - money2.FiveDollarCount,
+                money1.TwentyDollarCount - money2.TwentyDollarCount
             );
         }
 
