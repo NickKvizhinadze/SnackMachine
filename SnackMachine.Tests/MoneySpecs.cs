2b35ddf2de492cc39c28d41fe842e4a6c8756140
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2020-02-06 13:43:59 +0400

    Added Tests project and Money Specs tests

---
+++ SnackMachine.Tests/MoneySpecs.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+using SnackMachine.Logic;
+
+namespace SnackMachine.Tests
+{
+    public class MoneySpecs
+    {
+        [Fact]
+        public void Sum_of_two_moneys_produces_correct_result()
+        {
+            var money1 = new Money(1, 2, 3, 4, 5, 6);
+            var money2 = new Money(1, 2, 3, 4, 5, 6);
+            var sum = money1 + money2;
+
+            sum.OneCentCount.Should().Be(2);
+            sum.TenCentCount.Should().Be(4);
+            sum.QuarterCount.Should().Be(6);
+            sum.OneDollarCount.Should().Be(8);
+            sum.FiveDollarCount.Should().Be(10);
+            sum.TwentyDollarCount.Should().Be(12);
+        }
+
+        [Fact]
+        public void Two_moneys_instances_equal_if_contain_the_same_money_amount()
+        {
+            var money1 = new Money(1, 2, 3, 4, 5, 6);
+            var money2 = new Money(1, 2, 3, 4, 5, 6);
+
+            money1.Should().Be(money2);
+        }
+
+        [Fact]
+        public void Two_moneys_instances_not_equal_if_contain_the_different_money_amount()
+        {
+            var money1 = new Money(0, 0, 0, 1, 0, 0);
+            var money2 = new Money(100, 0, 0, 0, 0, 0);
+
+            money1.Should().NotBe(money2);
+        }
+
+        [Theory]
+        [InlineData(-1, 0, 0, 0, 0, 0)]
+        [InlineData(0, -1, 0, 0, 0, 0)]
+        [InlineData(0, 0, -1, 0, 0, 0)]
+        [InlineData(0, 0, 0, -1, 0, 0)]
+        [InlineData(0, 0, 0, 0, -1, 0)]
+        [InlineData(0, 0, 0, 0, 0, -1)]
+        public void Cannot_create_money_with_negative_value(
+            int oneCentCount,
+            int tenCentCount,
+            int quarterCount,
+            int oneDollarCount,
+            int fiveDollarCount,
+            int twentyDollarCount)
+        {
+            Action action = () => new Money(
+                oneCentCount,
+                tenCentCount,
+                quarterCount,
+                oneDollarCount,
+                fiveDollarCount,
+                twentyDollarCount);
+
+            action.Should().Throw<InvalidOperationException>();
+        }
+    }
+}
