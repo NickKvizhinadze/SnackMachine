d270faebd6daea40ec53640d63dd19f2d422a43c
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2020-02-06 11:25:17 +0400

    Added Entity base class

---
+++ SnackMachine.Logic/Entity.cs
@@ -0,0 +1,45 @@
+﻿namespace SnackMachine.Logic
+{
+    public abstract class Entity
+    {
+        public long Id { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Entity other))
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (GetType() != other.GetType())
+                return false;
+
+            if (Id == 0 || other.Id == 0)
+                return false;
+
+            return Id == other.Id;
+        }
+
+        public static bool operator ==(Entity entity1, Entity entity2)
+        {
+            if (entity1 is null && entity2 is null)
+                return true;
+
+            if (entity1 is null || entity2 is null)
+                return false;
+
+            return entity1.Equals(entity2);
+        }
+
+        public static bool operator !=(Entity entity1, Entity entity2)
+        {
+            return !(entity1 == entity2);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().ToString() + Id).GetHashCode();
+        }
+    }
+}
--- SnackMachine.Logic/SnackMachine.cs
+++ SnackMachine.Logic/SnackMachine.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SnackMachine.Logic
+﻿namespace SnackMachine.Logic
 {
-    public sealed class SnackMachine
+    public sealed class SnackMachine: Entity
     {
         public Money MoneyInside { get; set; }
 
