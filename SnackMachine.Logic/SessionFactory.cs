4e247649a5a06d302fc6cf2898ebfab03a0269bd
Nick Kvizhinadze <nickkvizhinadze@gmail.com>
2020-02-07 12:41:58 +0400

    Added Hibernate

---
+++ SnackMachine.Logic/SessionFactory.cs
@@ -0,0 +1,73 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using FluentNHibernate.Conventions;
+using FluentNHibernate.Conventions.AcceptanceCriteria;
+using FluentNHibernate.Conventions.Helpers;
+using FluentNHibernate.Conventions.Instances;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnackMachine.Logic
+{
+    public class SessionFactory
+    {
+        #region Fields
+        private static ISessionFactory _factory;
+        #endregion
+
+        #region Methods
+        internal static ISession OpenSession()
+        {
+            return _factory.OpenSession();
+        }
+
+        public static void Init(string connectionString)
+        {
+            _factory = BuildSessionFactory(connectionString);
+        }
+        #endregion
+
+        #region Private Methods
+        private static ISessionFactory BuildSessionFactory(string connectionString)
+        {
+            FluentConfiguration configuration = Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2012.ConnectionString(connectionString))
+                .Mappings(m => m.FluentMappings
+                    .AddFromAssembly(Assembly.GetExecutingAssembly())
+                    .Conventions.Add(
+                        ForeignKey.EndsWith("ID"),
+                        ConventionBuilder.Property
+                        .When(criteria => criteria.Expect(x => x.Nullable, Is.Not.Set), x => x.Not.Nullable()))
+                    .Conventions.Add<TableNameConvention>()
+                    .Conventions.Add<HiloConvention>()
+                );
+
+            return configuration.BuildSessionFactory();
+        }
+        #endregion
+
+        #region Classes
+        public class TableNameConvention: IClassConvention
+        {
+            public void Apply(IClassInstance instance)
+            {
+                instance.Table("[dbo].[" + instance.EntityType.Name + "]");
+            }
+        }
+
+        public class HiloConvention : IIdConvention
+        {
+            public void Apply(IIdentityInstance instance)
+            {
+                instance.Column(instance.EntityType.Name + "ID");
+                instance.GeneratedBy.HiLo("[dbo][Ids]", "NextHigh", "9", "EntityName = '" + instance.EntityType.Name + "'");
+            }
+        }  
+        #endregion
+    }
+}
